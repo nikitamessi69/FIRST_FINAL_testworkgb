0bf02c91da6cbbf0f361674084954d9a3dba153a
nikitamessi69 <nzhuravlev92@gmail.com>
2023-03-09 18:56:06 +0300

    Добавлено решение задачи в файле Program.cs

--- Task/Program.cs
+++ Task/Program.cs
@@ -5,5 +5,57 @@
 Примеры:
 ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
 ['1234", "1567", "-2", "computer science"] -> ["-2"]
-["Russia", "Denmark", "Kazan"] -> [
+["Russia", "Denmark", "Kazan"] -> []
 */
+
+
+string[] array = { "hello", "2", "world", ":-)", "1234", "1567",
+"-2", "computer science", "Russia", "Denmark", "Kazan" };
+
+int count = FindArray(array);
+int FindArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) count++;
+    }
+    return count;
+}
+
+string[] FillArray(string[] array, int count)
+{
+    string[] result = new string[count];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            result[result.Length - count] = array[i];
+            count--;
+        }
+    }
+    return result;
+}
+
+void PrintArray(string[] array)
+{
+    {
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.Write("[");
+        Console.ResetColor();
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]);
+            if (i < array.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("]");
+        Console.ResetColor();
+    }
+}
+
+string[] resultArray = FillArray(array, count);
+PrintArray(resultArray);
